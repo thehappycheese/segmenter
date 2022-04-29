cf45c3407eafdf14c74fa36c7101895aa80ab53b
thehappycheese <nicholas.archer@mainroads.wa.gov.au>
2022-04-29 16:08:50 +0800

    split_rows_by_segmentation passes test without slk

--- src/segmenter/_util/split_rows_by_segmentation.py
+++ src/segmenter/_util/split_rows_by_segmentation.py
@@ -2,12 +2,17 @@ import pandas
 import numpy as np
 from .check_monotonically_increasing_segments import check_monotonically_increasing_segments
 
-def _check_column_present(name, df, column_names, df_name):
+def _check_columns_present(name, df, column_names, df_name):
     if not all(item in df.columns for item in column_names):
         raise ValueError(f"{name} {[column_name for column_name in column_names if column_name not in df.columns]} not in `{df_name}`. Did you mean to use `{df_name}.reset_index(drop=False)`?")
 
 
-
+class CN:
+    original_index = "original_index"
+    original_df_num = "original_df_num"
+    additional_index = "additional_index"
+    event_measure = "event_measure"
+    event_type = "event_type"
 
 def split_rows_by_segmentation(
         original_segmentation:pandas.DataFrame,
@@ -42,30 +47,80 @@ def split_rows_by_segmentation(
         name_additional_index:  The desired name of the column that will be output into result. The value in this column will be the integer index of the row in `original_segmentation` that corresponds to each row of the `result`. Typically `'additional_index'`
     """
     
-    _check_column_present("categories",   original_segmentation, categories,   "original_segmentation")
-    _check_column_present("measure_slk",  original_segmentation, measure_slk,  "original_segmentation")
-    _check_column_present("measure_true", original_segmentation, measure_true, "original_segmentation")
-    
-    _check_column_present("categories",   additional_segmentation, categories,   "additional_segmentation")
-    _check_column_present("measure_slk",  additional_segmentation, measure_slk,  "additional_segmentation")
-    _check_column_present("measure_true", additional_segmentation, measure_true, "additional_segmentation")
+    if (name_original_index==name_additional_index):
+        raise ValueError(f"`name_original_index` and `name_additional_index` cannot be the same: {name_original_index}")
 
-    if name_original_index in original_segmentation.columns:
-        raise ValueError(f"name_original_index '{name_original_index}' already in original_segmentation")
-    if name_additional_index in original_segmentation.columns:
-        raise ValueError(f"name_additional_index '{name_additional_index}' already in original_segmentation")
-
-    check_monotonically_increasing_segments(original_segmentation, categories, measure_slk)
-    check_monotonically_increasing_segments(additional_segmentation, categories, measure_slk)
-
-    result = (
+    oseg = (
         original_segmentation
+        .reset_index(drop=False)
+        .loc[:,[*categories, *measure_slk, *measure_true]]
+        .assign(**{
+            CN.original_index: lambda df: pandas.RangeIndex(len(df.index)),
+            CN.original_df_num:0
+        })
+        .copy()
+    )
+    
+
+    aseg = (
+        additional_segmentation
+        .reset_index(drop=False)
+        .loc[:,[*categories, *measure_slk, *measure_true]]
+        .assign(**{
+            CN.original_index: lambda df: pandas.RangeIndex(len(df.index)),
+            CN.original_df_num:1
+        })
         .copy()
     )
 
-    result[name_original_index] = pandas.RangeIndex(len(result))
 
+    _check_columns_present("categories",   oseg, categories,   "original_segmentation")
+    _check_columns_present("measure_slk",  oseg, measure_slk,  "original_segmentation")
+    _check_columns_present("measure_true", oseg, measure_true, "original_segmentation")
     
+    _check_columns_present("categories",   aseg, categories,   "additional_segmentation")
+    _check_columns_present("measure_slk",  aseg, measure_slk,  "additional_segmentation")
+    _check_columns_present("measure_true", aseg, measure_true, "additional_segmentation")
 
-    return result
+    # TODO: these checks are expensive, are they strictly needed?
+    check_monotonically_increasing_segments(oseg, categories, measure_slk)
+    check_monotonically_increasing_segments(aseg, categories, measure_slk)
+
+    # TODO: will cause wierd error if one of the slk/true  from/to are swapped. implement a check.
+
+    from_events = pandas.concat([oseg, aseg])
+    from_events[CN.event_measure] = from_events[measure_true[0]]
+    from_events = from_events.sort_values([CN.event_measure, CN.original_df_num], ascending=[True, True], kind="stable")
+    from_events[CN.event_type] = "from"
+
+    to_events = pandas.concat([oseg, aseg])
+    to_events[CN.event_measure] = to_events[measure_true[1]]
+    to_events = to_events.sort_values([CN.event_measure, CN.original_df_num], ascending=[True, False], kind="stable")
+    to_events[CN.event_type] = "to"
+
+    events = pandas.concat([from_events, to_events]).sort_values([*categories, CN.event_measure, CN.original_index], kind="stable")#.sort_values([*categories,"event_measure"], kind="stable")
+
+    results = []
+    for group_index, group in events.groupby(categories):
+        segments = []
+        index_o = None
+        index_a = None
+        last_measure = None
+        for row_index, row in group.iterrows():
+            if not last_measure is None and row[CN.event_measure] - last_measure > 0 and not(index_o is None and index_a is None):
+                segments.append([*group_index, last_measure, row[CN.event_measure], index_o, index_a])
+            if row[CN.event_type] == "from":
+                if row[CN.original_df_num] == 0:
+                    index_o = row[CN.original_index]
+                else:
+                    index_a = row[CN.original_index]
+            elif row[CN.event_type] == "to":
+                if row[CN.original_df_num] == 0:
+                    index_o = None
+                else:
+                    index_a = None
+            last_measure = row[CN.event_measure]
+        results += segments
+
+    return pandas.DataFrame(data=results, columns=[*categories, *measure_true, name_original_index, name_additional_index])
 
--- test/test_split_rows_by_segmentation.py
+++ test/test_split_rows_by_segmentation.py
@@ -17,6 +17,8 @@ def test_split_rows_by_segmentation_without_poes():
 
             ["H002", "R", "L4", 0.000, 0.010, 0.000, 0.010, "r"],
             ["H002", "R", "L4", 0.010, 0.030, 0.010, 0.030, "s"],
+
+            ["H002", "R", "L4", 0.050, 0.080, 0.050, 0.080, "t"],
         ]
     )
 
@@ -26,49 +28,48 @@ def test_split_rows_by_segmentation_without_poes():
             ["H001", "L", "L1", 0.030, 0.045, 0.010, 0.025, "z"],
             ["H001", "L", "L1", 0.045, 0.055, 0.025, 0.035, "y"],
             
-            ["H001", "L", "L2", 0.030, 0.065, 0.010, 0.0, "x"],
+            ["H001", "L", "L2", 0.030, 0.065, 0.010, 0.045, "x"],
             
             ["H002", "R", "L4", 0.005, 0.012, 0.005, 0.012, "w"],
             ["H002", "R", "L4", 0.015, 0.040, 0.015, 0.040, "u"],
+
+            ["H002", "R", "L4", 0.052, 0.080, 0.052, 0.080, "v"],
         ]
     )
-    
     
     result = split_rows_by_segmentation(
         original_segmentation   = original_segmentation,
         additional_segmentation = additional_segmentation,
         categories              = ["road_no", "carriageway", "xsp"],
         measure_slk             = ("slk_from", "slk_to"),
-        measure_true            = ("true_from","true_to"),
+        measure_true            = ("true_from", "true_to"),
         name_original_index     = "original_index",
         name_additional_index   = "additional_index",
     )
-    
-    #result = result.drop(columns="old_index")
 
-
-    
     expected_result = pd.DataFrame(
-        columns=["road_no", "carriageway", "xsp", "slk_from", "slk_to", "true_from", "true_to", "value", "original_index", "additional_index"],
+        columns=["road_no", "carriageway", "xsp", "slk_from", "slk_to", "true_from", "true_to", "original_index", "additional_index"],
         data=[
-            ["H001", "L", "L1", 0.030, 0.040, 0.010, 0.020,    "a",      0,      0],
-            ["H001", "L", "L1", 0.040, 0.045, 0.020, 0.025,    "b",      1,      0],
-            ["H001", "L", "L1", 0.045, 0.050, 0.025, 0.030,    "b",      1,      1],
-            ["H001", "L", "L1", 0.050, 0.055, 0.030, 0.035, np.nan, np.nan,      1],
-            ["H001", "L", "L2", 0.030, 0.045, 0.010, 0.025,    "e",      2,      2],
-            ["H001", "L", "L2", 0.045, 0.060, 0.025, 0.040,    "f",      3,      2],
-            ["H001", "L", "L2", 0.060, 0.065, 0.040, 0.045, np.nan,      0,      2],
-            ["H002", "R", "L4", 0.000, 0.005, 0.000, 0.005,    "r",      4, np.nan],
-            ["H002", "R", "L4", 0.005, 0.010, 0.005, 0.010,    "r",      4,      3],
-            ["H002", "R", "L4", 0.010, 0.012, 0.010, 0.012,    "s",      5,      3],
-            ["H002", "R", "L4", 0.012, 0.015, 0.012, 0.015,    "s",      5,      4],
-            ["H002", "R", "L4", 0.015, 0.030, 0.015, 0.030,    "s",      5,      4],
-            ["H002", "R", "L4", 0.030, 0.040, 0.030, 0.040, np.nan, np.nan,      4],
+            ["H001", "L", "L1", 0.030, 0.040, 0.010, 0.020,      0,      0],
+            ["H001", "L", "L1", 0.040, 0.045, 0.020, 0.025,      1,      0],
+            ["H001", "L", "L1", 0.045, 0.050, 0.025, 0.030,      1,      1],
+            ["H001", "L", "L1", 0.050, 0.055, 0.030, 0.035, np.nan,      1],
+            ["H001", "L", "L2", 0.030, 0.045, 0.010, 0.025,      2,      2],
+            ["H001", "L", "L2", 0.045, 0.060, 0.025, 0.040,      3,      2],
+            ["H001", "L", "L2", 0.060, 0.065, 0.040, 0.045, np.nan,      2],
+            ["H002", "R", "L4", 0.000, 0.005, 0.000, 0.005,      4, np.nan],
+            ["H002", "R", "L4", 0.005, 0.010, 0.005, 0.010,      4,      3],
+            ["H002", "R", "L4", 0.010, 0.012, 0.010, 0.012,      5,      3],
+            ["H002", "R", "L4", 0.012, 0.015, 0.012, 0.015,      5, np.nan],
+            ["H002", "R", "L4", 0.015, 0.030, 0.015, 0.030,      5,      4],
+            ["H002", "R", "L4", 0.030, 0.040, 0.030, 0.040, np.nan,      4],
+            ["H002", "R", "L4", 0.050, 0.052, 0.050, 0.052,      6, np.nan],
+            ["H002", "R", "L4", 0.052, 0.080, 0.052, 0.080,      6,      5],
         ]
     )
 
     pd.testing.assert_frame_equal(
         result,
-        expected_result,
-        check_like =False, # ignore column order and label order
+        expected_result.drop(columns=["slk_from", "slk_to"]),
+        check_like=False, # ignore column order and label order
     )
