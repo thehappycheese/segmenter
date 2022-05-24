0b0bd89cc60b5d7a1fbcaff5979701818218be55
thehappycheese <nicholas.archer@mainroads.wa.gov.au>
2022-05-24 11:04:31 +0800

    test split_rows_by_segmentation preserves index failing

--- test/test_split_rows_by_segmentation.py
+++ test/test_split_rows_by_segmentation.py
@@ -168,3 +168,79 @@ def test_split_rows_by_segmentation_with_poes():
         expected_result,
         check_like=False, # ignore column order and label order
     )
+
+
+
+def test_split_rows_by_segmentation_preserves_indexes():
+    import pandas as pd
+    import numpy as np
+    from segmenter import split_rows_by_segmentation
+    
+    original_segmentation = pd.DataFrame(
+        columns=["road_no", "carriageway", "xsp", "slk_from", "slk_to", "true_from", "true_to", "value"],
+        data=[
+            ["H001", "L", "L1", 0.030, 0.040, 0.010, 0.020, "a"],
+            ["H001", "L", "L1", 0.040, 0.050, 0.020, 0.030, "b"],
+            
+            ["H001", "L", "L2", 0.030, 0.045, 0.010, 0.025, "c"],
+            ["H001", "L", "L2", 0.045, 0.060, 0.025, 0.040, "d"],
+
+            ["H002", "R", "L4", 0.000, 0.010, 0.000, 0.010, "e"],
+            ["H002", "R", "L4", 0.010, 0.030, 0.010, 0.030, "f"],
+
+            ["H002", "R", "L4", 0.050, 0.080, 0.050, 0.080, "g"],
+        ]
+    ).set_index("value")
+
+    additional_segmentation = pd.DataFrame(
+        columns=["road_no", "carriageway", "xsp", "slk_from", "slk_to", "true_from", "true_to", "value"],
+        data=[
+            ["H001", "L", "L1", 0.030, 0.045, 0.010, 0.025, "z"],
+            ["H001", "L", "L1", 0.045, 0.055, 0.025, 0.035, "y"],
+            
+            ["H001", "L", "L2", 0.030, 0.065, 0.010, 0.045, "x"],
+            
+            ["H002", "R", "L4", 0.005, 0.012, 0.005, 0.012, "w"],
+            ["H002", "R", "L4", 0.015, 0.040, 0.015, 0.040, "u"],
+
+            ["H002", "R", "L4", 0.052, 0.080, 0.052, 0.080, "v"],
+        ]
+    ).set_index("value")
+    
+    # TODO: this test will fail since currently the indexes are replaced by a fresh RangeIndex
+    result = split_rows_by_segmentation(
+        original_segmentation   = original_segmentation,
+        additional_segmentation = additional_segmentation,
+        categories              = ["road_no", "carriageway", "xsp"],
+        measure_slk             = ("slk_from", "slk_to"),
+        measure_true            = ("true_from", "true_to"),
+        name_original_index     = "original_index",
+        name_additional_index   = "additional_index",
+    )
+
+    expected_result = pd.DataFrame(
+        columns=["road_no", "carriageway", "xsp", "slk_from", "slk_to", "true_from", "true_to", "original_index", "additional_index"],
+        data=[
+            ["H001", "L", "L1", 0.030, 0.040, 0.010, 0.020,    "a",     "z"],
+            ["H001", "L", "L1", 0.040, 0.045, 0.020, 0.025,    "b",     "z"],
+            ["H001", "L", "L1", 0.045, 0.050, 0.025, 0.030,    "b",     "y"],
+            ["H001", "L", "L1", 0.050, 0.055, 0.030, 0.035, np.nan,     "y"],
+            ["H001", "L", "L2", 0.030, 0.045, 0.010, 0.025,    "c",     "x"],
+            ["H001", "L", "L2", 0.045, 0.060, 0.025, 0.040,    "d",     "x"],
+            ["H001", "L", "L2", 0.060, 0.065, 0.040, 0.045, np.nan,     "x"],
+            ["H002", "R", "L4", 0.000, 0.005, 0.000, 0.005,    "e",  np.nan],
+            ["H002", "R", "L4", 0.005, 0.010, 0.005, 0.010,    "e",     "w"],
+            ["H002", "R", "L4", 0.010, 0.012, 0.010, 0.012,    "f",     "w"],
+            ["H002", "R", "L4", 0.012, 0.015, 0.012, 0.015,    "f",  np.nan],
+            ["H002", "R", "L4", 0.015, 0.030, 0.015, 0.030,    "f",     "u"],
+            ["H002", "R", "L4", 0.030, 0.040, 0.030, 0.040, np.nan,     "u"],
+            ["H002", "R", "L4", 0.050, 0.052, 0.050, 0.052,    "g",  np.nan],
+            ["H002", "R", "L4", 0.052, 0.080, 0.052, 0.080,    "g",     "v"],
+        ]
+    )
+
+    pd.testing.assert_frame_equal(
+        result,
+        expected_result,
+        check_like=False, # ignore column order and label order
+    )
